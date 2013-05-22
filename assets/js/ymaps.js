cab1955af3b5ba0a75fd49f12da8e170f452629b
Alexander Kolosov <akolosov@cs.karelia.ru>
2013-05-22 14:45:40 +0400

    Set placemarks on the map from a prepaired array.

--- assets/js/ymaps.js
+++ assets/js/ymaps.js
@@ -5,39 +5,56 @@ var nearObjects = [
     {
         "location": "Петрозаводск, ул. Торнева, 2",
         "icon": 'twirl#redStretchyIcon',
-        "label": "магазин Ленторг"
+        "label": "Магазин Ленторг"
+    },
+    {
+        "location": "Петрозаводск, ул. Парфенова, 5",
+        "icon": 'twirl#redStretchyIcon',
+        "label": "Магазин Магнит"
     },
     {
         "location": "Петрозаводск, ул. Балтийская, 11А",
         "icon": 'twirl#blueStretchyIcon',
-        "label": "детсад"
+        "label": "Детсад"
+    },
+    {
+        "location": "Петрозаводск, ул. Парфенова, 8А",
+        "icon": 'twirl#blueStretchyIcon',
+        "label": "Школа №2"
+    },
+    {
+        "location": "Петрозаводск, ул. Ровио, 14",
+        "icon": 'twirl#blueStretchyIcon',
+        "label": "Поликлиника"
     },
 ];
+
+// Function to set point corresponding to i-th description
+// Currying hack from http://www.svendtofte.com/code/curried_javascript/
+function setPoint(i, map, res) {
+    if (arguments.length < 3) {
+        return function(res) {
+            var obj = res.geoObjects.get(0);
+            obj.options.set('preset', nearObjects[i].icon);
+            obj.properties.set('iconContent', nearObjects[i].label);
+            map.geoObjects.add(res.geoObjects);
+        }
+    }
+}
 
 function rovioMapInit() {
     rovioMap = new ymaps.Map ("map", {
         center: [61.765891, 34.376584],
         zoom: 16,
     });
-
     
     rovioMap.controls.add('mapTools');
     rovioMap.controls.add('smallZoomControl');
 
     for (var i in nearObjects) {      
-        var icon = nearObjects[i].icon;
-        var label = nearObjects[i].label;
-
         var geocoder = ymaps.geocode(nearObjects[i].location);
     
-        geocoder.then(
-            function (res) {
-                alert(res);
-                var obj = res.geoObjects.get(0);
-                obj.options.set('preset', icon);
-                obj.properties.set('iconContent', i);
-                rovioMap.geoObjects.add(res.geoObjects);
-            });
+        geocoder.then(setPoint(i, rovioMap));
     }
 }
     
