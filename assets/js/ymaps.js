3b70d6fbff256ec96fda35d14586ced791e08d8f
Alexander Kolosov <akolosov@gmail.com>
2013-05-25 21:58:05 +0400

    Bus stations added on map. Link to street panorama added.

--- assets/js/ymaps.js
+++ assets/js/ymaps.js
@@ -4,19 +4,24 @@ var rovioMap;
 var nearObjects = [
     {
         "location": "Петрозаводск, ул. Ровио, 8",
-        "icon": 'twirl#blueDotIcon',
-        "label": "Трехкомнатная квартира"        
+        "icon": 'twirl#blueStretchyIcon',
+        "label": "ул. Ровио, д. 8",
+        "hasIconText": true,
     },
-    // {
-    //     "location": [61.765727,34.377867],
-    //     "icon": 'twirl#trolleybusIcon',
-    //     "label": "Остановка общественного транспорта (из города)"        
-    // },    
-    // {
-    //     "location": [61.765627,34.37785],
-    //     "icon": 'twirl#trolleybusIcon',
-    //     "label": "Остановка общественного транспорта (в город)"        
-    // },    
+    {
+        
+        "location": [61.765589,34.3797],
+        "icon": 'twirl#trolleybusIcon',
+        "isPreciseLocation": true,
+        "label": "Остановка общественного транспорта (из города)"
+    },    
+    {
+        
+        "location": [61.765619,34.377704],
+        "icon": 'twirl#trolleybusIcon',
+        "isPreciseLocation": true,
+        "label": "Остановка общественного транспорта (в город)"        
+    },    
     {
         "location": "Петрозаводск, ул. Торнева, 2",
         "icon": 'twirl#shopIcon',
@@ -36,13 +41,19 @@ var nearObjects = [
         "location": "Петрозаводск, ул. Балтийская, 11А",
         "icon": 'twirl#greenStretchyIcon',
         "hasIconText": true,
-        "label": "Детсад"
+        "label": "Детский<br/>сад"
     },
     {
         "location": "Петрозаводск, ул. Парфенова, 8А",
         "icon": 'twirl#greenStretchyIcon',
         "hasIconText": true,
         "label": "Школа №2"
+    },
+    {
+        "location": "Петрозаводск, ул. Парфенова, 8",
+        "icon": 'twirl#greenStretchyIcon',
+        "hasIconText": true,
+        "label": "Музыкальная<br/>школа №3"
     },
     {
         "location": "Петрозаводск, ул. Лыжная, 22А",
@@ -58,6 +69,11 @@ var nearObjects = [
         "location": "Петрозаводск, ул. Ровио, 13",
         "icon": 'twirl#hospitalIcon',
         "label": "Детская поликлиника №2"
+    },
+    {
+        "location": "Петрозаводск, ул. Ровио, 12А",
+        "icon": 'twirl#hospitalIcon',
+        "label": "Аптека"
     },
     {
         "location": "Петрозаводск, ул. Парфенова, 2",
@@ -79,7 +95,8 @@ function setPoint(i, map, res) {
             var obj = res.geoObjects.get(0);
             obj.options.set('preset', nearObjects[i].icon);
             if (nearObjects[i].hasIconText) {
-                obj.properties.set('iconContent', nearObjects[i].label);
+                obj.properties.set('iconContent', '<span style="font-size: smaller">' +
+                                   nearObjects[i].label + '</span>');
             } else {
                 obj.properties.set('hintContent', nearObjects[i].label);
             }
@@ -90,7 +107,7 @@ function setPoint(i, map, res) {
 
 function rovioMapInit() {
     rovioMap = new ymaps.Map ("map", {
-        center: [61.766032,34.377867],
+        center: [61.766595,34.377962],
         zoom: 16,
     });
     
@@ -98,9 +115,18 @@ function rovioMapInit() {
     rovioMap.controls.add('smallZoomControl');
 
     for (var i in nearObjects) {      
-        var geocoder = ymaps.geocode(nearObjects[i].location, {results: 25});
+        if (nearObjects[i].isPreciseLocation) {
+            var point = new ymaps.Placemark(nearObjects[i].location, {                
+                hintContent: nearObjects[i].label
+            }, {
+                preset: nearObjects[i].icon,
+            });
+            rovioMap.geoObjects.add(point);
+        } else {
+            var geocoder = ymaps.geocode(nearObjects[i].location, {results: 25});
     
-        geocoder.then(setPoint(i, rovioMap));
+            geocoder.then(setPoint(i, rovioMap));
+        }
     }
 }
     
